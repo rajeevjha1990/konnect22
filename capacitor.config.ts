264b477aa2847fb0c406bc1431d21bbc42814f71
Rajeev Jha <rajeevjha1990@gmail.com>
2026-05-25 14:26:40 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'kennect22-new',
+  webDir: 'www'
+};
+
+export default config;
